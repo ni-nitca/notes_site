ef9d546e8fb9cf779653b91a3a4c05e2c7c6b0eb
paigni <ikontai4@gmail.com>
2022-12-24 21:25:42 +0600

    переписана функция регистр сэйв

--- notes/notes_site/service.py
+++ notes/notes_site/service.py
@@ -1,51 +1,42 @@
 from notes_site.models import (
     Note,
-    Hash,
+    EmailHash,
+    User,
     Authorize,
     Registration,
-    User
 )
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from notes_site.check import (
+    check_reg_password,
+    check_register_data,
 
-def check_register_json(file_of_json):
-    if file_of_json != []:
-        values = file_of_json.keys()
-        true_list = [
-            "username",
-            "email",
-            "password1",
-            "password2",
-        ]
-        for value in true_list:
-            if value not in values:
-                return False
-        return True
-    else:
-        return False
+)
+import uuid
 
-def register_save(req):
+
+
+def register_save(data):
     user = User()
-    if req.method == "POST":
-        data = req.POST
-        if check_register_json(data):
-            user.username = data['username']
-            user.email = data['email']
-            user.password = data['password1']  #check пароля будет производится на фронте?
+    hash = EmailHash()
+    if not check_register_data(data) and not check_reg_password(data):
+        return False
+    user.first_name = data.get('first_name')
+    user.last_name = data.get('last_name')
+    user.email = data.get('email')
+    user.password = data.get('password1')
+    hash_text = uuid(4)
+    hash.hash_text = hash_text
+
+    user.save()
+    hash.save()
+    return hash_text
 
 
 
-def autorize(req):
-    if req.method == 'POST':
-        data = req.POST
-        user = authenticate(username=data['username'],password=data['password'])
-        if user is not None:
-            if user.is_active:
-                return login(req,user)
-            else:
-                return HttpResponse('Аккаунт не активирован')
-        else:
-            return HttpResponse('Неверный логин или пароль')
-    else:
-        return render (req,template_name)
+
+    
+        
+        
+
 
