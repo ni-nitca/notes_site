4396c139c6884c665e2a10bf7d15ecd4e9f51259
paigni <ikontai4@gmail.com>
2022-12-24 21:22:26 +0600

    правки

--- notes/notes_site/admin.py
+++ notes/notes_site/admin.py
@@ -2,22 +2,20 @@ from django.contrib import admin
 from solo.admin import SingletonModelAdmin
 from notes_site.models import Note, Authorize, Registration
 
-
+@admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     class Meta:
         model = Note
         fields = ["title", "description ", "posted_date","tags"]
 
+@admin.register(Authorize)
 class AuthorizeAdmin(SingletonModelAdmin):
     class Meta:
         model = Authorize
         fields = ['description']
 
+@admin.register(Registration)
 class RegisterAdmin(SingletonModelAdmin):
     class Meta:
         model = Registration
         fields = ['description']
-
-admin.site.register(Note, NoteAdmin)
-admin.site.register(Authorize, AuthorizeAdmin)
-admin.site.register(Registration, RegisterAdmin)
