8dd0d51ec45d8d0baba9906677603f9fd00ada17
paigni <ikontai4@gmail.com>
2022-12-24 21:25:17 +0600

    добавлена модель пользователя

--- notes/notes_site/models.py
+++ notes/notes_site/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
 from solo.models import SingletonModel
-
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 from taggit.managers import TaggableManager
 
 
@@ -30,7 +31,61 @@ class Note(models.Model):
         verbose_name_plural = "Заметки"
 
 
-class Hash(models.Model):
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField('first name', max_length=30, blank=True)
+    last_name = models.CharField('last name', max_length=30, blank=True)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+    is_active = models.BooleanField('active', default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.user.email
+
+
+class EmailHash(models.Model):
     user = models.OneToOneField(
         User,
         verbose_name="Хэш",
@@ -40,6 +95,9 @@ class Hash(models.Model):
         verbose_name="Тест хэша",
         max_length=64,
     )
+
+    def __str__(self):
+        return self.user.email
 
     class Meta:
         db_table = "hash"
