409e3f234cc1c9f6839d06d9cb0143fe35e4bf03
paigni <ikontai4@gmail.com>
2022-12-26 20:23:39 +0600

    Новые функции send_email,activation,get_notes переделана функция register

--- notes/notes_site/service.py
+++ notes/notes_site/service.py
@@ -12,31 +12,81 @@ from notes_site.check import (
     check_register_data,
 
 )
-import uuid
+
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage 
+from django.contrib.sites.shortcuts import get_current_site
 
 
 
-def register_save(data):
+def register_save(data,request):
     user = User()
-    hash = EmailHash()
-    if not check_register_data(data) and not check_reg_password(data):
-        return False
+    if not check_register_data(data):
+        answer = {
+            "code":400,
+            "text": "Неверно передан словарь"
+        }
+        return answer
+    if not check_reg_password(data):
+        answer = {
+            "code":200,
+            "text":"Пароль не равны между собой"
+        }
+        return answer
     user.first_name = data.get('first_name')
     user.last_name = data.get('last_name')
     user.email = data.get('email')
     user.password = data.get('password1')
-    hash_text = uuid(4)
-    hash.hash_text = hash_text
 
+    EmailHash.objects.create(user=user)
     user.save()
-    hash.save()
-    return hash_text
+    hash = EmailHash.objects.filter(user=user)
+    email_send(data,request,user,hash)
+    answer = {
+        "answer":200,
+        "text":"Все прошло успешно"
+    }
+    return answer
 
 
+def email_send(data,request,user,hash):
+    current_site = get_current_site(request) 
+    mail_subject = 'Ссылка активации отправлена на вашу электронную почту'
+    message = render_to_string('acc_active_email.html', { 
+                'user': user, 
+                'domain': current_site.domain, 
+                'url': hash
+            })
+    to_email = data.get('email')
+    email = EmailMessage(
+            mail_subject,
+            message,
+            to=to_email
+        )
+    email.send
+    return 'Пожалуйста проверьте вашу почту для завершения регистрации'
 
-
-    
+def activation(request):
+    user = User()
+    hash = EmailHash.objects.filter(user=user)
+    if user is not None and hash is not None:
+        user.is_active = True
+        user.save()
+        answer = {
+            "code":200,
+            "text":"Вы успешно активировали ваш аккаунт"
+        }
+        return answer
+    else:
+        answer = {
+            "code":404,
+            "text":"Некорректаная ссылка"
+        }
+        return answer
         
-        
 
-
+def get_notes(request):
+    user = request.user
+    notes = Note.objects.filter(user_id = user)
+    data = {"notes":notes}
+    return data
