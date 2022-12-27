35b7c2606854194500fc6ffbd08c9cbf605f963a
paigni <ikontai4@gmail.com>
2022-12-27 20:07:44 +0600

    Добавлен тест

--- notes/notes_site/tests.py
+++ notes/notes_site/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+from notes_site.models import (
+    Note,
+    EmailHash,
+    MailSettings
+)
+
+from notes_site.service import (
+    register_save,
+    email_send
+)
+
+
+class ServiceTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.data = {"email": 'mr@mail.ru', "password1": '12345', "password2": '12345'}
+
+    def test_register_save(self):
+        data = self.data
+        answer = answer = {
+        "answer":200,
+        "text":"Все прошло успешно"
+        }
+        self.assertEqual(answer, register_save(data))
+
+    def     
+
