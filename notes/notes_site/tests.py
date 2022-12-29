5af4ecbb46b7f5d4e50c210161fc477c222fb461
paigni <ikontai4@gmail.com>
2022-12-29 20:19:55 +0600

    Добавлен тест регистрации

--- notes/notes_site/tests.py
+++ notes/notes_site/tests.py
@@ -16,14 +16,19 @@ class ServiceTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.data = {"email": 'mr@mail.ru', "password1": '12345', "password2": '12345'}
-
+        cls.data1 = {"email": 'mr@mail.ru', "password1": '12345', "password2": '12345'}
     def test_register_save(self):
         data = self.data
-        answer = answer = {
-        "answer":200,
+        data1 = self.data1
+        answer = {
+        "status_code":200,
         "text":"Все прошло успешно"
         }
+        answer1 = {
+            "status_code":402,
+            "text":"Ваша почта уже используется"
+        }
         self.assertEqual(answer, register_save(data))
+        self.assertEqual(answer1, register_save(data1))
 
-    def     
 
