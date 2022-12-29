5262ec239b02d1671a1e76e04742d87b28a85b65
paigni <ikontai4@gmail.com>
2022-12-29 20:20:50 +0600

    Убрал taggit

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTH_USER_MODEL = 'notes_site.User'
@@ -41,8 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "notes_site",
-    "taggit",
-    "solo"
+    "solo",
 ]
 
 MIDDLEWARE = [
