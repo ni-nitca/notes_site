33f9108d1ab0cf33ca3aa7b0d06a593ad8c7aba8
paigni <ikontai4@gmail.com>
2023-01-04 20:18:33 +0600

    Добавлены тесты

--- notes/notes_site/tests.py
+++ notes/notes_site/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
 
 from notes_site.models import (
+    User,
     Note,
+    Tags,
     EmailHash,
     MailSettings
 )
 
 from notes_site.service import (
     register_save,
-    email_send
+    email_send,
+    get_notes
 )
 
 
@@ -17,18 +20,57 @@ class ServiceTestCase(TestCase):
     def setUpTestData(cls):
         cls.data = {"email": 'mr@mail.ru', "password1": '12345', "password2": '12345'}
         cls.data1 = {"email": 'mr@mail.ru', "password1": '12345', "password2": '12345'}
+        cls.email = "mr@mail.ru"
+        cls.is_superuser = False
+        cls.is_staff = False
+        cls.is_active = True
+        cls.get_data = {"tags":"1,3,4,6"}
+        cls.title = "123"
+        cls.description = "321"
+        cls.slug = "123"
+        cls.data2 = []
+
     def test_register_save(self):
         data = self.data
-        data1 = self.data1
         answer = {
-        "status_code":200,
-        "text":"Все прошло успешно"
+          "status_code":200,
+          "text":"Все прошло успешно"
         }
-        answer1 = {
+        self.assertEqual(answer, register_save(data))
+
+        data = self.data1
+        answer = {
             "status_code":402,
             "text":"Ваша почта уже используется"
         }
         self.assertEqual(answer, register_save(data))
-        self.assertEqual(answer1, register_save(data1))
 
+    def test_get_notes(self):
+        user = User.objects.create(
+            email = self.email,
+            password = 12345,
+            is_active = True
+        )
+        note = Note.objects.create (
+            user = user,
+            title = self.title,
+            description = self.description,
+            slug = self.slug
+        )
+        answer = {"notes":note}
+        self.assertEqual(answer, get_notes(user,data=[]))
+        tags = self.get_data.get("tags")
+        tags_list = tags.split(',')
+        obj = [
+        Tags(
+            note = note,
+            tag = tag, 
+        )
+        for tag in tags_list
+        ]
+        create_notes = Tags.objects.bulk_create(obj)
+        notes = Note.objects.filter(user_id = user)
+        note = notes.filter(tags__in = tags_list)
+        answer = {"notes":note}
+        self.assertEqual(answer, get_notes(user,self.get_data))
 
