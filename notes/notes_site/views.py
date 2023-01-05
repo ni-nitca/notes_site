cc4ffd54a93c56b465b3e42092ab89f311394a68
paigni <ikontai4@gmail.com>
2023-01-05 20:20:31 +0600

    Добавлена функция indexview

--- notes/notes_site/views.py
+++ notes/notes_site/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import login
 from notes_site.service import (
     register_save,
     activation,
     authorization,
+    get_context_auth,
+    get_context_reg,
+    get_notes,
+    edit_notes
     )
 from django.core.mail import EmailMessage 
 from notes_site.models import User
 from django.views.generic import (
+    View,
     ListView,
 )
 
 
 
-class Autorize(ListView):
-    def get(self,request):#context?
+class IndexView(View):
+    def get(self, request):
+        template_name = ''
+        context = get_context_auth()
+        return render(request, template_name, context)
+
+    def post(self, request):
+        if request.user.is_authenticated():
+            template_name = ''
+            return HttpResponseRedirect(template_name)
+        if "reg" in request.POST:
+        template_name = ''
+        context = get_context()
+        context['saved'] = saved
+        return render(request, template_name, context)
+
+
+class Autorize(View):
+    def get(self,request):
         template_name = ""
         return render(request,template_name)
 
@@ -28,9 +50,9 @@ class Autorize(ListView):
             login(request,auth)
 
 
-class Register(ListView):
+class Register(View):
     def get(self,request):
-        template_name = 'notes_site/signup.html'
+        template_name = 'notes_site/.html'
         return render(request, template_name)
 
     def post(self,request):
@@ -45,17 +67,17 @@ class Register(ListView):
             return render(request,template_name,text)        
          
 
-class Activate(ListView):
-    def get(self,request):
-        template_name = ''
-        hash = request.GET.get('hash')
-        answer = activation(hash)
-        status_code = answer.get("status_code")
-        text = answer.get("text")
-        if status_code == 400:
-            return HttpResponse(text)
-        else:
-            return render(request,template_name)
+
+def activate(request):
+    template_name = ''
+    hash = request.GET.get('hash')
+    answer = activation(hash)
+    status_code = answer.get("status_code")
+    text = answer.get("text")
+    if status_code == 400:
+        return HttpResponse(text)
+    else:
+        return render(request,template_name)
 
 
 
