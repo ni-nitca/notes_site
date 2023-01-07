8da37a122a2aa1217e9a70039202f6b9d5b43672
paigni <ikontai4@gmail.com>
2023-01-07 19:36:57 +0600

    изменены пути

--- notes/notes_site/urls.py
+++ notes/notes_site/urls.py
@@ -12,11 +12,11 @@ from notes_site.views import (
 
 
 urlpatterns = [
-    path('home/', IndexView.as_view(), name = 'home'),
-    path('home/', AutorizeView.as_view(), name = 'authorize'),
-    path('home/', RegisterView.as_view(), name = 'registrarion'),
+    path('', IndexView.as_view(), name = 'home'),
+    path('auth/', AutorizeView.as_view(), name = 'authorize'),
+    path('register/', RegisterView.as_view(), name = 'registrarion'),
     path('activate/<str:hash>', ActivateView.as_view(), name = 'activate'),
-    path('note/<str:slugify>',NoteDetailView.as_view(),name = 'note-detail'),
+    path('note/<slug:slug>',NoteDetailView.as_view(),name = 'note-detail'),
     path('note/new', NoteCreateView.as_view(), name = 'note-create'),
     path('note/<str:slugify>/delete',NoteDeleteView.as_view(),name = 'delete-view'),
 ]
