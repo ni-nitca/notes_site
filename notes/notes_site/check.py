ee94499f7432fd2ff9666f2416c293e6a8f78512
paigni <ikontai4@gmail.com>
2023-01-16 20:26:27 +0600

    Правки,удалён чек

--- notes/notes_site/check.py
+++ notes/notes_site/check.py
@@ -1,5 +1,5 @@
 def check_reg_password(data:dict):
-    if not check_register_data(data):
+    if not isinstance(data, dict):
         return False
 
     password1 = data.get('password1')
@@ -10,7 +10,7 @@ def check_reg_password(data:dict):
 
 
 def check_restore_data(data:dict):
-    if not check_register_data(data):
+    if not isinstance(data, dict):
         return False
 
     values = data.keys()
@@ -41,18 +41,6 @@ def check_register_data(data: dict):
     return True
 
 
-def check_restore_password(data: dict):
-    if not isinstance(data, dict):
-        return False
-    values = data.keys()
-    true_list = [
-        "email",
-    ]
-    if values not in true_list:
-        return False
-    return True
-
-
 def check_authorize_data(data:dict):
     if not isinstance(data, dict):
         return False
@@ -73,9 +61,10 @@ def check_notes(data:dict):
 
     values = data.keys()
     true_list = [
-        "slug"
+        "slug",
         "title",
         "description",
+        "tags"
     ]
     for value in true_list:
         if value not in values:
